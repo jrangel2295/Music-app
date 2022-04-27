882e09bc47000f7498d91d650b584d2c0a7c4a05
Johnny Rangel <jrangel2295@gmail.com>
2022-04-26 19:29:15 -0500

    first commit

---
+++ music-app/src/App.js
@@ -0,0 +1,14 @@
+
+import Card from './components/Card/Card';
+import Header from './components/Header/Header'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1 className='welcome-text'>Welcome johnny</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
